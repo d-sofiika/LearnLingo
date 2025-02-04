923f001a8c6ede07540566e30514a584a4d7ae28
d-sofiika <soniadrozd2709@gmail.com>
2025-02-04 15:19:05 +0200

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+
+import LogoUkraine from "../../../public/LogoUkraine.svg";
+import css from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={`section ${css.headerContainer}`}>
+      <nav>
+        <NavLink to="/" className={css.navLogo}>
+          <img src={LogoUkraine} className={css.logo} alt="Logo" />
+          <p className={css.logoText}>LearnLingo</p>
+        </NavLink>
+      </nav>
+
+      <nav className={css.nav}>
+        <NavLink to="/" className={css.link}>
+          Home
+        </NavLink>
+        <NavLink to="/teachers" className={css.link}>
+          Teachers
+        </NavLink>
+      </nav>
+      <div className={css.buttonBox}>
+        <button type="button" className={css.btnLogIn}>
+          <svg width="26" height="24" className={css.logIcon}>
+            <use href="/sprite.svg#icon-log-in"></use>
+          </svg>
+          <p>Log in</p>
+        </button>
+        <button type="button" className={css.btnRegist}>
+          Registration
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
-import css from "./HomePage.module.css"
+import Header from "../../components/Header/Header";
+
 
 const HomePage = () => {
   return (
     <div className="container">
-        <p className={css.pp}>jjjj</p>
+        <Header/>
     </div>
   );
 };
